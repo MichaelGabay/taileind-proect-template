df43b0d40d300cc15b1972efb4ce2e41213676e9
Micael Gabay <michaelgabay770@gmail.com>
2024-04-02 13:33:10 +0300

    create dog paper

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {}
+    extend: {
+      colors:{
+        pinkButton:"#f84071"
+      }
+    }
   },
   plugins: [],
 }
